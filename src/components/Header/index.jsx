c251daae2539d861036e8c38a7d4b357020c6bcd
Ndiongmo <mauricendiongmo@gmail.com>
2022-12-21 18:22:41 +0100

    Scopez votre CSS avec styled components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,51 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import logo from '../../assets/dark-logo.png'
+
+const HeaderHeader = styled.header`
+    width: 90%;
+    display: flex;
+    flex-direction: row;
+    height: 50px;
+    margin: auto;
+`
+
+const HeaderImage = styled.img`
+    width: 100px;
+    height: 50px;
+    display: flex;
+`
+
+const HeaderNav = styled.nav`
+    display: flex;
+    margin-left: auto;
+    flex-wrap: wrap;
+    gap: 15px;
+`
+
+const StyledLink = styled(Link)`
+    padding: 15px;
+    color: #8186a0;
+    text-decoration: none;
+    font-size: 18px;
+    ${(props) =>
+        props.$isFullLink &&
+        `color: white; border-radius: 30px; background-color: ${colors.primary}`}
+`
 
 function Header() {
     return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/survey/1">Questionnaire</Link>
-            <Link to="/freelances">Freelances</Link>
-        </nav>
+        <HeaderHeader>
+            <HeaderImage src={logo} alt="Shiny Agency" />
+            <HeaderNav>
+                <StyledLink to="/">Accueil</StyledLink>
+                <StyledLink to="/freelances">Profils</StyledLink>
+                <StyledLink to="/survey/1" $isFullLink>
+                    Faire le test
+                </StyledLink>
+            </HeaderNav>
+        </HeaderHeader>
     )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,74 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import colors from '../../utils/style/colors'
+import homeIllustration from '../../assets/home-illustration.svg'
+
+const HomeContainer = styled.div`
+    width: 90%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    background: #f9f9fc;
+    margin: auto;
+    margin-top: 50px;
+`
+
+const HomeAbout = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 80px 40px 80px 40px;
+`
+
+const Balloon = styled.div`
+    height: 100px;
+    width: 100px;
+    border-radius: 50px;
+    background-color: #e20202;
+    transform: scale(${({ size }) => size});
+`
+
+const StyledLink = styled(Link)`
+    padding: 15px;
+    color: #8186a0;
+    text-decoration: none;
+    font-size: 18px;
+    ${(props) =>
+        props.$isFullLink &&
+        `color: white; border-radius: 30px; background-color: ${colors.primary}`}
+`
+
 function Home() {
+    const [size, setsize] = useState(1)
     return (
-        <div>
-            <h1> Page d'accueil 🏠</h1>
-        </div>
+        <HomeContainer>
+            <HomeAbout>
+                <div>
+                    <h1
+                        style={{
+                            marginBottom: '25px',
+                            fontSize: '55px',
+                            fontWeight: '50px',
+                        }}
+                    >
+                        Repérez vos besoins, <br />
+                        on s’occupe du reste, <br />
+                        avec les meilleurs talents
+                    </h1>
+                    <StyledLink to="/survey/1" $isFullLink>
+                        Faire le test
+                    </StyledLink>
+                </div>
+                <div>
+                    <img src={homeIllustration} alt="Talent" />
+                </div>
+            </HomeAbout>
+            <div>
+                <h1 onClick={() => setsize(size + 0.1)}> Page d'accueil 🏠</h1>
+                <Balloon size={size} />
+            </div>
+        </HomeContainer>
     )
 }
 
