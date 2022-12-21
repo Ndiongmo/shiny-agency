bfeed361347e4eab8c19f7d1d4a6dd0a43d6f77c
Ndiongmo <mauricendiongmo@gmail.com>
2022-12-21 14:30:07 +0100

    Indiquez les types de vos props avec les PropTypes

---
+++ src/pages/Freelances/index.jsx
@@ -0,0 +1,38 @@
+import DefaultPicture from '../../assets/profile.png'
+import Card from '../../components/Card'
+
+const freelanceProfiles = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+        picture: DefaultPicture,
+    },
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Développeur frontend',
+        picture: DefaultPicture,
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+        picture: DefaultPicture,
+    },
+]
+
+function Freelances() {
+    return (
+        <div>
+            <h1>Freelances 😷 💻 </h1>
+            {freelanceProfiles.map((profile, index) => (
+                <Card
+                    key={`${profile.name}-${index}`}
+                    label={profile.jobTitle}
+                    picture={profile.picture}
+                    title={profile.name}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default Freelances
